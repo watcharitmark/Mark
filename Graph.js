b454f44f2aa16fd84925ab7e03e260a0a7a0671f
watcharitmark <57636824+watcharitmark@users.noreply.github.com>
2020-04-17 13:34:56 +0700

    Add files via upload

---
+++ Graph.js
@@ -0,0 +1,31 @@
+{
+  "$schema": "https://vega.github.io/schema/vega-lite/v4.json",
+  "data": {
+      "url": "https://raw.githubusercontent.com/watcharitmark/Mark/master/_inf_ปริมาณมูลฝอย61.csv"
+  },
+  
+  "title": "ปริมาณมูลฝอยปี61",
+
+
+  "height": {"step": 15},
+  "mark": "bar",
+  "encoding": {
+    "y": {
+      "field": "dname",
+      "type": "nominal",
+      "title": "เขต",
+      "sort":"-x"
+    },
+    "x": {
+      "field": "Average",
+      "type": "quantitative",
+      "axis": {"title": "ค่าเฉลี่ยปริมาณมูลฝอยในปี2561"}
+    },
+    "color":{"field":"dname",
+            "title":"เขต"},
+
+
+    "tooltip": [
+                {"field": "Average", "type": "quantitative"}]
+  }
+}
